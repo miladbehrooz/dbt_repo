ec3904981b3e444793a3575683df865fd424405d
Milad Behrooz <milad@spiced-academy.com>
2023-09-01 21:44:39 +0200

    update staging folder

--- models/staging/staging_temp.sql
+++ models/staging/staging_temp.sql
@@ -8,7 +8,7 @@ WITH temperature_daily AS (
            (extracted_data -> 'location' -> 'country')::VARCHAR  AS country,
            ((extracted_data -> 'location' -> 'lat')::VARCHAR)::NUMERIC  AS lat, 
            ((extracted_data -> 'location' -> 'lon')::VARCHAR)::NUMERIC  AS lon
-    FROM {{source("staging", "temp_test")}})
+    FROM {{source("staging", "raw_temmp")}})
 SELECT * 
 FROM temperature_daily
 
